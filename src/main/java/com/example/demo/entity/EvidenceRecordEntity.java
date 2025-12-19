a3b814921dbd9d491062e48c4a87d5b79aaa41fc
bala-amypo <bala@amypo.in>
2025-12-19 01:58:57 +0000

    User commit

--- src/main/java/com/example/demo/entity/EvidenceRecordEntity.java
+++ src/main/java/com/example/demo/entity/EvidenceRecordEntity.java
@@ -1,4 +1,25 @@
 package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+
+@Entity
 public class EvidenceRecordEntity{
-    
+    @Id
+    @GeneratedValue(strategy=GenerationType.Identity)
+    private Long id;
+
+    private String evidenceType;
+    private String content;
+    private String submittedBy;
+    private LocalDateTime submittedAt;
+
+    public EvidenceRecordEntity{}
+
+    public EvidenceRecordEntity()
 }
