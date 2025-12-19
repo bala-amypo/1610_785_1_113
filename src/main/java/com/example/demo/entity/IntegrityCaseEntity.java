613e3a1bee0e57f5d5339b1d60a9e098ae2813ef
bala-amypo <bala@amypo.in>
2025-12-19 01:37:57 +0000

    User commit

--- src/main/java/com/example/demo/entity/IntegrityCaseEntity.java
+++ src/main/java/com/example/demo/entity/IntegrityCaseEntity.java
@@ -2,5 +2,13 @@ package com.example.demo.entity;
 
 @Entity
 public class IntegrityCaseEntity{
-    
+    @Id
+    @GeneratedValue(strategy=GenerationType.Identity)
+    private Long id;
+    private String courseCode;
+    private String instructorName;
+    private String description;
+    private String status;
+    private LocalDate incidentDate;
+    private LocalDateTime createdAt;
 }
