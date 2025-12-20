f4adaf4563951515025049b7fd7c23d6458a241d
bala-amypo <bala@amypo.in>
2025-12-20 06:50:49 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentProfileService.java
+++ src/main/java/com/example/demo/service/StudentProfileService.java
@@ -4,6 +4,6 @@ import com.example.demo.entity.StudentProfileEntity;
 public interface StudentProfileService{
      StudentProfileEntity postData(StudentProfileEntity stu);
      List<StudentProfileEntity>getAllData(); 
-     StudentProfileEntity 
+     StudentProfileEntity  getStudent
 
 }
