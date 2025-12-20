1bc0b4c947771d02d414cd6d795d8db74f82c412
bala-amypo <bala@amypo.in>
2025-12-20 07:26:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentProfileController.java
+++ src/main/java/com/example/demo/controller/StudentProfileController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation;
-import org.springframework.web.bind.annotation;
-import org.springframework.web.bind.annotation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.service.StudentProfileService;
 import com.example.demo.entity.StudentProfileEntity;
