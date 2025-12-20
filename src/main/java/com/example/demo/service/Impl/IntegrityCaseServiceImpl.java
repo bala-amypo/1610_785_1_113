ba56559e2e3d25c2350d1698477813e43372c5c7
bala-amypo <bala@amypo.in>
2025-12-20 11:05:35 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/IntegrityCaseServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/IntegrityCaseServiceImpl.java
@@ -1,4 +1,43 @@
 package com.example.demo.service.Impl;
-public class IntegrityCaseServiceImpl{
-    
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.IntegrityCaseEntity;
+import com.example.demo.repository.IntegrityCaseRepository;
+import com.example.demo.service.IntegrityCaseService;
+
+@Service
+public class IntegrityCaseServiceImpl implements IntegrityCaseService{
+        @Autowired IntegrityCaseRepository repo;
+
+        @Override
+        public IntegrityCaseEntity postData(IntegrityCaseEntity stu){
+            return repo.save(stu);
+        }
+
+        @Override
+        public List<IntegrityCaseEntity>getAllData(){
+            return repo.findAll();
+        }
+
+        @Override
+        public IntegrityCaseEntity getStudentById(Long id){
+            return repo.findById(id).orElse(null);
+        }
+
+        @Override
+        public IntegrityCaseEntity updateRepeatStatus(String studentId,Boolean status){
+            IntegrityCaseEntity student=repo.findByStudentId(studentId).orElse(null);
+            if(student !=null){
+                student.setRepeatOffender(status);
+                return repo.save(student);
+            }
+            return null;
+        }
+        @Override
+        public IntegrityCaseEntity getByStudentIdentifier(String studentId){
+            return repo.findByStudentId(studentId).orElse(null);
+        }   
 }
