536da4915aa2ac2a0326605dee904640f43ea85d
bala-amypo <bala@amypo.in>
2025-12-20 05:51:06 +0000

    User commit

--- src/main/java/com/example/demo/entity/EvidenceRecordEntity.java
+++ src/main/java/com/example/demo/entity/EvidenceRecordEntity.java
@@ -1,64 +1,64 @@
-package com.example.demo.entity;
+// package com.example.demo.entity;
 
-import java.time.LocalDateTime;
+// import java.time.LocalDateTime;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
 
 
-@Entity
-public class EvidenceRecordEntity{
-    @Id
-    @GeneratedValue(strategy=GenerationType.Identity)
-    private Long id;
+// @Entity
+// public class EvidenceRecordEntity{
+//     @Id
+//     @GeneratedValue(strategy=GenerationType.Identity)
+//     private Long id;
 
-    private String evidenceType;
-    private String content;
-    private String submittedBy;
-    private LocalDateTime submittedAt;
+//     private String evidenceType;
+//     private String content;
+//     private String submittedBy;
+//     private LocalDateTime submittedAt;
 
-    public Long getId(){
-        return id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-    public String getEvidenceType(){
-        return evidenceType;
-    }
-    public void setEvidenceType(String evidenceType){
-        this.evidenceType=evidenceType;
-    }
-    public String getContent(){
-        return content;
-    }
-    public void setcontent(String content){
-        this.content=content;
-    }
-    public String getSubmittedBy(){
-        return submittedBy;
-    }
-    public void setSubmittedBy(String submittedBy){
-        this.submittedBy=submittedBy;
-    }
-    public LocalDateTime getSubmittedAt(){
-        return  submittedAt;
-    }
-    public void setSubmittedAt(LocalDateTime submittedAt){
-        this.submittedAt=submittedAt;
-    }
+//     public Long getId(){
+//         return id;
+//     }
+//     public void setId(Long id){
+//         this.id=id;
+//     }
+//     public String getEvidenceType(){
+//         return evidenceType;
+//     }
+//     public void setEvidenceType(String evidenceType){
+//         this.evidenceType=evidenceType;
+//     }
+//     public String getContent(){
+//         return content;
+//     }
+//     public void setcontent(String content){
+//         this.content=content;
+//     }
+//     public String getSubmittedBy(){
+//         return submittedBy;
+//     }
+//     public void setSubmittedBy(String submittedBy){
+//         this.submittedBy=submittedBy;
+//     }
+//     public LocalDateTime getSubmittedAt(){
+//         return  submittedAt;
+//     }
+//     public void setSubmittedAt(LocalDateTime submittedAt){
+//         this.submittedAt=submittedAt;
+//     }
 
-    public EvidenceRecordEntity{}
+//     public EvidenceRecordEntity{}
 
-    public EvidenceRecordEntity(Long id,String evidenceType,String content,
-    String submittedBy,LocalDateTime submittedAt){
-        this.id=id;
-        this.evidenceType=evidenceType;
-        this.content=content;
-        this.submittedBy=submittedBy;
-        this.submittedAt=submittedAt;
-    }
+//     public EvidenceRecordEntity(Long id,String evidenceType,String content,
+//     String submittedBy,LocalDateTime submittedAt){
+//         this.id=id;
+//         this.evidenceType=evidenceType;
+//         this.content=content;
+//         this.submittedBy=submittedBy;
+//         this.submittedAt=submittedAt;
+//     }
 
-}
+// }
--- src/main/java/com/example/demo/entity/IntegrityCaseEntity.java
+++ src/main/java/com/example/demo/entity/IntegrityCaseEntity.java
@@ -1,81 +1,81 @@
-package com.example.demo.entity;
+// package com.example.demo.entity;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
+// import java.time.LocalDate;
+// import java.time.LocalDateTime;
+// import java.util.List;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
 
-@Entity
-public class IntegrityCaseEntity{
-    @Id
-    @GeneratedValue(strategy=GenerationType.Identity)
-    private Long id;
-    private String courseCode;
-    private String instructorName;
-    private String description;
-    private String status;
-    private LocalDate incidentDate;
-    private LocalDateTime createdAt;
+// @Entity
+// public class IntegrityCaseEntity{
+//     @Id
+//     @GeneratedValue(strategy=GenerationType.Identity)
+//     private Long id;
+//     private String courseCode;
+//     private String instructorName;
+//     private String description;
+//     private String status;
+//     private LocalDate incidentDate;
+//     private LocalDateTime createdAt;
 
-    public Long getId(){
-        return id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-    public String getCourseCode(){
-         return courseCode;
-    }
-    public void setCourseCode(String courseCode){
-         this.courseCode=courseCode;
+//     public Long getId(){
+//         return id;
+//     }
+//     public void setId(Long id){
+//         this.id=id;
+//     }
+//     public String getCourseCode(){
+//          return courseCode;
+//     }
+//     public void setCourseCode(String courseCode){
+//          this.courseCode=courseCode;
 
-    }
-    public String getInstructorName(){
-          return instructorName;
-    }
-    public void setInstructorName(String instructorName){
-          this.instructorName=instructorName;
-    }
-    public  string getDescription(){
-        return description;
-    }
-    public void setDescription(String description){
-        this.description=description;
-    }
-    public String getStatus(){
-        return status;
-    }
-    public void setStatus(String status){
-        this.status=status;
-    }
-    public LocalDate getIncidentDate(){
-        return  incidentDate;
-    }
-    public void setIncidentDate(LocalDate incidentDate){
-        this.incidentDate=incidentDate;
-    }
-    public LocalDateTime getCreatedAt(){
-        return  createdAt;
-    }
-    public void setCreatedAt(LocalDateTime createdAt){
-         this.createdAt=createdAt;
-    }
+//     }
+//     public String getInstructorName(){
+//           return instructorName;
+//     }
+//     public void setInstructorName(String instructorName){
+//           this.instructorName=instructorName;
+//     }
+//     public  string getDescription(){
+//         return description;
+//     }
+//     public void setDescription(String description){
+//         this.description=description;
+//     }
+//     public String getStatus(){
+//         return status;
+//     }
+//     public void setStatus(String status){
+//         this.status=status;
+//     }
+//     public LocalDate getIncidentDate(){
+//         return  incidentDate;
+//     }
+//     public void setIncidentDate(LocalDate incidentDate){
+//         this.incidentDate=incidentDate;
+//     }
+//     public LocalDateTime getCreatedAt(){
+//         return  createdAt;
+//     }
+//     public void setCreatedAt(LocalDateTime createdAt){
+//          this.createdAt=createdAt;
+//     }
 
 
-    public IntegrityCaseEntity{}
+//     public IntegrityCaseEntity{}
 
-    public IntegrityCaseEntity(Long id,String courseCode,String instructorName,
-    String description,String status,LocalDate incidentDate,LocalDateTime createdAt){
-        this.id=id;
-        this.courseCode=courseCode;
-        this.instructorName=instructorName;
-        this.description=description;
-        this.status=status;
-        this.incidentDate=incidentDate;
-        this.createdAt=createdAt;
-    }
-}
+//     public IntegrityCaseEntity(Long id,String courseCode,String instructorName,
+//     String description,String status,LocalDate incidentDate,LocalDateTime createdAt){
+//         this.id=id;
+//         this.courseCode=courseCode;
+//         this.instructorName=instructorName;
+//         this.description=description;
+//         this.status=status;
+//         this.incidentDate=incidentDate;
+//         this.createdAt=createdAt;
+//     }
+// }
