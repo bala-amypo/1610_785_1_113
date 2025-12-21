b77315c7be72445e529b3d368fc98ff97d96d3b0
bala-amypo <bala@amypo.in>
2025-12-21 13:21:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/RepeatOffenderRecord.java
+++ src/main/java/com/example/demo/entity/RepeatOffenderRecord.java
@@ -1,11 +1,9 @@
-
-
-
-
 package com.example.demo.entity;
-import java.time.LocalDate;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "repeat_offender_record")
@@ -15,27 +13,55 @@ public class RepeatOffenderRecord {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "Student profile is mandatory")
     @ManyToOne
     @JoinColumn(name = "student_id")
     private StudentProfile studentProfile;
 
+    @NotNull(message = "Total cases is mandatory")
+    @Min(value = 1, message = "Total cases must be at least 1")
     private Integer totalCases;
+
+    @NotNull(message = "Last incident date is mandatory")
     private LocalDate lastIncidentDate;
+
+    @NotBlank(message = "Flag severity is mandatory")
     private String flagSeverity;
 
-    // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    
+    public Long getId() {
+         return id; 
+    }
+    public void setId(Long id) {
+         this.id = id; 
+    }
 
-    public StudentProfile getStudentProfile() { return studentProfile; }
-    public void setStudentProfile(StudentProfile studentProfile) { this.studentProfile = studentProfile; }
+    public StudentProfile getStudentProfile() {
+         return studentProfile;
+    }
+    public void setStudentProfile(StudentProfile studentProfile) { 
+        this.studentProfile = studentProfile; 
+    }
 
-    public Integer getTotalCases() { return totalCases; }
-    public void setTotalCases(Integer totalCases) { this.totalCases = totalCases; }
+    public Integer getTotalCases() {
+         return totalCases; 
+    }
+    public void setTotalCases(Integer totalCases) {
+         this.totalCases = totalCases; 
+    }
 
-    public LocalDate getLastIncidentDate() { return lastIncidentDate; }
-    public void setLastIncidentDate(LocalDate lastIncidentDate) { this.lastIncidentDate = lastIncidentDate; }
+    public LocalDate getLastIncidentDate() {
+         return lastIncidentDate; 
+    }
+    public void setLastIncidentDate(LocalDate lastIncidentDate) { 
+        this.lastIncidentDate = lastIncidentDate; 
+    }
 
-    public String getFlagSeverity() { return flagSeverity; }
-    public void setFlagSeverity(String flagSeverity) { this.flagSeverity = flagSeverity; }
+    public String getFlagSeverity() {
+         return flagSeverity; 
+    }
+    public void setFlagSeverity(String flagSeverity) {
+         this.flagSeverity = flagSeverity; 
+    }
+
 }
