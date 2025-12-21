877c2f415480121f70af3bb8ff8539f5fb8be102
bala-amypo <bala@amypo.in>
2025-12-21 13:57:31 +0000

    User commit

--- src/main/java/com/example/demo/repository/IntegrityCaseRepository.java
+++ src/main/java/com/example/demo/repository/IntegrityCaseRepository.java
@@ -1,15 +1,3 @@
-// package com.example.demo.repository;
-// import java.util.List;
-
-// import org.springframework.stereotype.Repository;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import com.example.demo.entity.IntegrityCaseEntity;
-
-// @Repository
-// public interface IntegrityCaseRepository extends JpaRepository<IntegrityCaseEntity,Long>{
-//         List<IntegrityCaseEntity>findByStudentProfileStudentId(String studentId);
-// }
-    
 
  package com.example.demo.repository;
 
