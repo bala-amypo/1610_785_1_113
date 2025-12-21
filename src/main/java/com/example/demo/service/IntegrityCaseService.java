6ed921da71fcd232e22d93fa6623e784e3170669
bala-amypo <bala@amypo.in>
2025-12-21 16:04:31 +0000

    User commit

--- src/main/java/com/example/demo/service/IntegrityCaseService.java
+++ src/main/java/com/example/demo/service/IntegrityCaseService.java
@@ -1,18 +1,3 @@
-// package com.example.demo.service;
-
-// import java.util.List;
-// import com.example.demo.entity.IntegrityCaseEntity;
-// public interface IntegrityCaseService{
-//      IntegrityCaseEntity postData(IntegrityCaseEntity stu);
-//      List<IntegrityCaseEntity>getAllData(); //get all
-//      IntegrityCaseEntity getStudentById(Long id);  //get by Id
-//      IntegrityCaseEntity updateRepeatStatus(Long id,String status); //put repeat
-//      List<IntegrityCaseEntity>getByStudentIdentifier(String studentId);  //lookup
-
-// }
-
-
-
 package com.example.demo.service;
 
 import java.util.List;
