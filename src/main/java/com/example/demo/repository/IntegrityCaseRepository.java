5c3bd87602f50650bbf7dce3b6ab06b0d133f6ee
bala-amypo <bala@amypo.in>
2025-12-25 04:55:57 +0000

    User commit

--- src/main/java/com/example/demo/repository/IntegrityCaseRepository.java
+++ src/main/java/com/example/demo/repository/IntegrityCaseRepository.java
@@ -1,20 +1,20 @@
-
- package com.example.demo.repository;
+package com.example.demo.repository;
 
 import java.time.LocalDate;
 import java.util.List;
-import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.demo.entity.IntegrityCase;
 import com.example.demo.entity.StudentProfile;
 
-@Repository
 public interface IntegrityCaseRepository extends JpaRepository<IntegrityCase, Long> {
 
     List<IntegrityCase> findByStudentProfile(StudentProfile studentProfile);
 
-    List<IntegrityCase> findByStatusAndIncidentDateAfter(String status, LocalDate date);
+    List<IntegrityCase> findByStudentIdentifier(String id);
+
+    List<IntegrityCase> findRecentCasesByStatus(String status, LocalDate date);
 
     List<IntegrityCase> findByIncidentDateBetween(LocalDate start, LocalDate end);
-} 
+}
--- src/main/java/com/example/demo/repository/StudentProfileRepository.java
+++ src/main/java/com/example/demo/repository/StudentProfileRepository.java
@@ -1,12 +1,12 @@
 package com.example.demo.repository;
 
 import java.util.Optional;
-import org.springframework.stereotype.Repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.demo.entity.StudentProfile;
 
-@Repository
 public interface StudentProfileRepository extends JpaRepository<StudentProfile, Long> {
 
-    Optional<StudentProfile> findByStudentId(String studentId);
-}
+    Optional<StudentProfile> findByStudentIdentifier(String id);
+}  
