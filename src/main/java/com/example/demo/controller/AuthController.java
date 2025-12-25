9dc1cf6277b930b83a26b02dc2a426014d831fb4
bala-amypo <bala@amypo.in>
2025-12-25 06:19:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,30 @@
+ package com.example.demo.controller;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.service.AuthService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService service;
+
+    public AuthController(AuthService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/register")
+    public void register(@Valid @RequestBody AuthRequest request) {
+        service.register(request);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@Valid @RequestBody AuthRequest request) {
+        return service.login(request);
+    }
+}
