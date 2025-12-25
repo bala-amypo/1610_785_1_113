8d648162fbd24a5d8b5ea51c473d3f331ffd5c0e
bala-amypo <bala@amypo.in>
2025-12-25 12:26:34 +0000

    User commit

--- src/main/java/com/example/demo/entity/AppUser.java
+++ src/main/java/com/example/demo/entity/AppUser.java
@@ -10,11 +10,18 @@ public class AppUser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
+    private String email;
+
     private String username;
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<Role> roles;
+
+    public AppUser() {
+    }
+
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
@@ -37,4 +44,5 @@ public class AppUser {
     }
 
 } 
+
 
