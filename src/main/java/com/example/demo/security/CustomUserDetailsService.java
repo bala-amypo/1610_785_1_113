73302507828303607b20f9a47ce8b563fa33e099
bala-amypo <bala@amypo.in>
2025-12-25 06:16:56 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -1,5 +1,43 @@
-package com.example.demo.security;
+// package com.example.demo.security;
 
+// import org.springframework.security.core.userdetails.*;
+// import org.springframework.stereotype.Service;
+
+// import com.example.demo.entity.AppUser;
+// import com.example.demo.repository.AppUserRepository;
+
+// @Service
+// public class CustomUserDetailsService implements UserDetailsService {
+
+//     private final AppUserRepository userRepo;
+
+//     public CustomUserDetailsService(AppUserRepository userRepo) {
+//         this.userRepo = userRepo;
+//     }
+
+//     @Override
+//     public UserDetails loadUserByUsername(String email)
+//             throws UsernameNotFoundException {
+
+//         AppUser user = userRepo.findByEmail(email)
+//                 .orElseThrow(() ->
+//                         new UsernameNotFoundException("User not found"));
+
+//         return User.builder()
+//                 .username(user.getEmail())
+//                 .password(user.getPassword())
+//                 .roles("USER")
+//                 .build();
+//     }
+// } 
+
+
+
+ package com.example.demo.security;
+
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 
@@ -21,12 +59,15 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         AppUser user = userRepo.findByEmail(email)
                 .orElseThrow(() ->
-                        new UsernameNotFoundException("User not found"));
+                    new UsernameNotFoundException("User not found"));
 
-        return User.builder()
-                .username(user.getEmail())
-                .password(user.getPassword())
-                .roles("USER")
-                .build();
+        return new User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(r -> new SimpleGrantedAuthority(r.getName()))
+                        .collect(Collectors.toSet())
+        );
     }
-} 
+}
+ 
