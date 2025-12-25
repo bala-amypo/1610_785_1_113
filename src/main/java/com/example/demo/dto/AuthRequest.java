ff9f1cffe4748bd9c5509a2aad36b1028d14efcb
bala-amypo <bala@amypo.in>
2025-12-25 16:21:36 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -1,36 +1,30 @@
 package com.example.demo.dto;
-import jakarta.validation.constraints.*;
+
+import jakarta.validation.constraints.NotBlank;
 
 public class AuthRequest {
+
     @NotBlank
-    @Email 
-    private String email; 
-    private String username;
+    private String email;
+
+    @NotBlank
     private String password;
 
     public AuthRequest() {}
 
-    public AuthRequest(String username, String password) {
-
-         
-        this.username = username;
-        this.password = password;
-    }
-    
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
-    public String getEmail() {      // ✅ ADD
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) { // ✅ ADD
+    public void setEmail(String email) {
         this.email = email;
     }
-} 
 
-
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}  
