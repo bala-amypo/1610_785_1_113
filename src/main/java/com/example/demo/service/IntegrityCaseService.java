e14661405ce5157c70d1080aa97c62bff9672bfd
bala-amypo <bala@amypo.in>
2025-12-26 00:46:54 +0000

    User commit

--- src/main/java/com/example/demo/service/IntegrityCaseService.java
+++ src/main/java/com/example/demo/service/IntegrityCaseService.java
@@ -1,18 +1,33 @@
+// package com.example.demo.service;
+
+// import java.util.List;
+// import java.util.Optional;
+// import com.example.demo.entity.IntegrityCase;
+
+// public interface IntegrityCaseService {
+
+//     IntegrityCase createCase(IntegrityCase integrityCase);
+
+//     IntegrityCase updateCaseStatus(Long caseId, String status);
+
+//     List<IntegrityCase> getCasesByStudent(Long studentId);
+
+//     Optional<IntegrityCase> getCaseById(Long id);
+
+//     List<IntegrityCase> getAllCases();
+// } 
+
+
 package com.example.demo.service;
 
+import com.example.demo.entity.IntegrityCase;
+import com.example.demo.entity.StudentProfile;
 import java.util.List;
 import java.util.Optional;
-import com.example.demo.entity.IntegrityCase;
 
 public interface IntegrityCaseService {
-
-    IntegrityCase createCase(IntegrityCase integrityCase);
-
+    IntegrityCase createCase(IntegrityCase c);
     IntegrityCase updateCaseStatus(Long caseId, String status);
-
+    Optional<IntegrityCase> getCaseById(Long caseId);
     List<IntegrityCase> getCasesByStudent(Long studentId);
-
-    Optional<IntegrityCase> getCaseById(Long id);
-
-    List<IntegrityCase> getAllCases();
-} 
+}
--- src/main/java/com/example/demo/service/StudentProfileService.java
+++ src/main/java/com/example/demo/service/StudentProfileService.java
@@ -1,17 +1,30 @@
+// package com.example.demo.service;
+
+// import java.util.List;
+// import com.example.demo.entity.StudentProfile;
+
+// public interface StudentProfileService {
+
+//     StudentProfile createStudent(StudentProfile studentProfile);
+
+//     StudentProfile getStudentById(Long id);
+
+//     List<StudentProfile> getAllStudents();
+
+//     StudentProfile updateRepeatOffenderStatus(Long studentId);
+
+//     StudentProfile getStudentByStudentIdentifier(String studentIdentifier); 
+// }  
+
+
 package com.example.demo.service;
 
-import java.util.List;
 import com.example.demo.entity.StudentProfile;
+import java.util.List;
 
 public interface StudentProfileService {
-
-    StudentProfile createStudent(StudentProfile studentProfile);
-
+    StudentProfile createStudent(StudentProfile s);
     StudentProfile getStudentById(Long id);
-
     List<StudentProfile> getAllStudents();
-
     StudentProfile updateRepeatOffenderStatus(Long studentId);
-
-    StudentProfile getStudentByStudentIdentifier(String studentIdentifier); 
-}  
+}
